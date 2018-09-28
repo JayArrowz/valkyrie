71b9c406c3eb563b7de2fa51555e1cb9aa9eb3b1
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2018-09-28 14:21:19 -0300

    Added License

--- src/test/java/com/github/marceloaguiarr/valkyrie/ValkyrieTest.java
+++ src/test/java/com/github/marceloaguiarr/valkyrie/ValkyrieTest.java
@@ -1,14 +1,28 @@
-import com.github.marceloaguiarr.valkyrie.Valkyrie;
+/*
+ * Copyright 2018 Marcelo Aguiar Rodrigues
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.marceloaguiarr.valkyrie;
+
 import com.github.marceloaguiarr.valkyrie.enums.SecurityManagers;
 import com.github.marceloaguiarr.valkyrie.managers.ThreadSecurityManager;
 import com.github.marceloaguiarr.valkyrie.profiles.NoPermissionsSecurityProfile;
 import com.github.marceloaguiarr.valkyrie.profiles.SecurityProfile;
 import org.junit.Test;
 
-import java.net.URL;
 import java.net.URLClassLoader;
 import java.security.Policy;
-import java.security.PrivilegedAction;
 
 import static org.junit.Assert.*;
 
