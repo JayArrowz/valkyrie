21593a556631ac37c43745b7fbdf16553263ce36
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2018-10-03 11:02:21 -0300

    Trigger build

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ extensions = [
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-language_dirs = ['_locale']
+language_dirs = ['locales']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
