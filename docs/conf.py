60c406c2382ca85ef22c3729ac33a49649604418
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2018-10-03 09:53:37 -0300

    Trying to add multiple languages

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,7 @@ extensions = [
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+language_dirs = ['_locale']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -58,7 +59,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en,pt"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
